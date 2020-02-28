ee6b57964744e12c8e0cff85503c8e0232f773ee
Becheru888 <becheru93@gmail.com>
2020-02-28 19:40:30 +0000

    home page icons added

--- marketing/src/pages/landing_page.js
+++ marketing/src/pages/landing_page.js
@@ -1,27 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 import Nav from "../components/navigation";
+import img from "../assets/icons.svg";
 
 function Landing() {
   return (
     <ThePage>
       <Nav />
-      <header>
-        <p className="statemant">
-          <span className="make">MAKE </span>
-          <span className="your">YOUR</span>
-          <br />
-          <span className="business">BUSSINES </span>
-          <span className="easyer">EASYER</span>
-        </p>
 
-        <p className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
-          rhoncus nulla. Maecenas sem lorem, aliquam fermentum metus et,
-          ultricies luctus augue. Curabitur aliquet maximus interdum. Nam congue
-          dictum fermentum.
-        </p>
-      </header>
+      <div>
+        <header>
+          <p className="statemant">
+            <span className="make">MAKE </span>
+            <span className="your">YOUR</span>
+            <br />
+            <span className="business">BUSINESS </span>
+            <span className="easyer">EASYER</span>
+          </p>
+          <p className="description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+            rhoncus nulla. Maecenas sem lorem, aliquam fermentum metus et,
+            ultricies luctus augue. Curabitur aliquet maximus interdum. Nam
+            congue dictum fermentum.
+          </p>
+        </header>
+        <section className="soft-png">
+          <img srcSet={`${img}`}></img>
+        </section>
+      </div>
     </ThePage>
   );
 }
@@ -30,14 +36,26 @@ export default Landing;
 
 const ThePage = styled.div`
   position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
   min-height: 100%;
 
-  header {
+  div {
+    display: flex;
+
+    .soft-png {
+      img {
+        position: relative;
+        width: 600px;
+        height: 600px;
+      }
+    }
+  }
+
+  div > header {
+    display: flex;
+    flex-flow: column;
     margin-top: 100px;
     width: 50%;
+
     .statemant {
       font-size: 4em;
       font-family: "Montserrat";
